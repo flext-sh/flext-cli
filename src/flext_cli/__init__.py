44dcca0dc9db562f28db6c314b96f9669ba59864
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-07-06 02:46:39 -0300

    chore(codegen): inline lazy __init__.py, eliminate _exports.py sidecar
    Regenerated by 'flext_infra codegen init --apply': PEP 562 lazy imports are
    now inlined directly into each package __init__.py; the separate _exports.py
    lazy-map sidecar is removed (superseded by the inline map).

--- src/flext_cli/__init__.py
+++ src/flext_cli/__init__.py
@@ -15,57 +15,114 @@ from flext_cli.__version__ import (
     __version__,
     __version_info__,
 )
-from flext_cli._exports import (
-    FLEXT_CLI_LAZY_IMPORTS,
-    FLEXT_CLI_PUBLIC_EXPORTS,
+from flext_core.lazy import (
+    build_lazy_import_map,
+    install_lazy_exports,
+    merge_lazy_imports,
 )
-from flext_core.lazy import install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_cli.api import FlextCli as FlextCli, cli as cli
-    from flext_cli.base import FlextCliServiceBase as FlextCliServiceBase, s as s
-    from flext_cli.constants import FlextCliConstants as FlextCliConstants, c as c
-    from flext_cli.models import FlextCliModels as FlextCliModels, m as m
-    from flext_cli.protocols import FlextCliProtocols as FlextCliProtocols, p as p
-    from flext_cli.services.auth import FlextCliAuth as FlextCliAuth
-    from flext_cli.services.cli import FlextCliCli as FlextCliCli
-    from flext_cli.services.cli_params import (
-        FlextCliCommonParams as FlextCliCommonParams,
-    )
-    from flext_cli.services.cmd import FlextCliCmd as FlextCliCmd
-    from flext_cli.services.file_tools import FlextCliFileTools as FlextCliFileTools
-    from flext_cli.services.formatters import FlextCliFormatters as FlextCliFormatters
-    from flext_cli.services.output import FlextCliOutput as FlextCliOutput
-    from flext_cli.services.pipeline import FlextCliPipeline as FlextCliPipeline
-    from flext_cli.services.rules import FlextCliRules as FlextCliRules
-    from flext_cli.services.runtime import FlextCliRuntime as FlextCliRuntime
-    from flext_cli.services.tables import FlextCliTables as FlextCliTables
-    from flext_cli.settings import FlextCliSettings as FlextCliSettings
-    from flext_cli.typings import FlextCliTypes as FlextCliTypes, t as t
-    from flext_cli.utilities import FlextCliUtilities as FlextCliUtilities, u as u
-    from flext_core import d as d, e as e, h as h, r as r, x as x
-
-
-_LAZY_IMPORTS = {
-    name: target
-    for name, target in FLEXT_CLI_LAZY_IMPORTS.items()
-    if name in FLEXT_CLI_PUBLIC_EXPORTS
-}
-
-
-_EAGER_EXPORTS = (
-    __author__,
-    __author_email__,
-    __description__,
-    __license__,
-    __title__,
-    __url__,
-    __version__,
-    __version_info__,
+    from flext_cli.api import FlextCli, cli
+    from flext_cli.base import FlextCliServiceBase, s
+    from flext_cli.constants import FlextCliConstants, c
+    from flext_cli.models import FlextCliModels, m
+    from flext_cli.protocols import FlextCliProtocols, p
+    from flext_cli.services.auth import FlextCliAuth
+    from flext_cli.services.cli import FlextCliCli
+    from flext_cli.services.cli_params import FlextCliCommonParams
+    from flext_cli.services.cmd import FlextCliCmd
+    from flext_cli.services.file_tools import FlextCliFileTools
+    from flext_cli.services.formatters import FlextCliFormatters
+    from flext_cli.services.output import FlextCliOutput
+    from flext_cli.services.pipeline import FlextCliPipeline
+    from flext_cli.services.rules import FlextCliRules
+    from flext_cli.services.runtime import FlextCliRuntime
+    from flext_cli.services.tables import FlextCliTables
+    from flext_cli.settings import FlextCliSettings
+    from flext_cli.typings import FlextCliTypes, t
+    from flext_cli.utilities import FlextCliUtilities, u
+    from flext_core._root_typing_parts.facades import d, e, h, r, x
+_LAZY_IMPORTS = merge_lazy_imports(
+    (".services",),
+    build_lazy_import_map(
+        {
+            ".api": (
+                "FlextCli",
+                "cli",
+            ),
+            ".base": (
+                "FlextCliServiceBase",
+                "s",
+            ),
+            ".constants": (
+                "FlextCliConstants",
+                "c",
+            ),
+            ".models": (
+                "FlextCliModels",
+                "m",
+            ),
+            ".protocols": (
+                "FlextCliProtocols",
+                "p",
+            ),
+            ".services._prompts_parts.flextcliprompts_part_03": ("FlextCliPrompts",),
+            ".services._prompts_parts.flextcliprompts_support": (
+                "FlextCliPromptsSupport",
+            ),
+            ".services.auth": ("FlextCliAuth",),
+            ".services.cli": ("FlextCliCli",),
+            ".services.cli_params": ("FlextCliCommonParams",),
+            ".services.cmd": ("FlextCliCmd",),
+            ".services.file_tools": ("FlextCliFileTools",),
+            ".services.formatters": ("FlextCliFormatters",),
+            ".services.output": ("FlextCliOutput",),
+            ".services.pipeline": ("FlextCliPipeline",),
+            ".services.rules": ("FlextCliRules",),
+            ".services.runtime": ("FlextCliRuntime",),
+            ".services.tables": ("FlextCliTables",),
+            ".settings": ("FlextCliSettings",),
+            ".typings": (
+                "FlextCliTypes",
+                "t",
+            ),
+            ".utilities": (
+                "FlextCliUtilities",
+                "u",
+            ),
+            "flext_core._root_typing_parts.facades": (
+                "d",
+                "e",
+                "h",
+                "r",
+                "x",
+            ),
+        },
+    ),
+    exclude_names=(
+        "_cli_parts",
+        "_prompts_parts",
+        "cleanup_submodule_namespace",
+        "install_lazy_exports",
+        "lazy_getattr",
+        "logger",
+        "merge_lazy_imports",
+        "output",
+        "output_reporting",
+        "pytest_addoption",
+        "pytest_collect_file",
+        "pytest_collection_modifyitems",
+        "pytest_configure",
+        "pytest_runtest_setup",
+        "pytest_runtest_teardown",
+        "pytest_sessionfinish",
+        "pytest_sessionstart",
+        "pytest_terminal_summary",
+        "pytest_warning_recorded",
+    ),
+    module_name=__name__,
 )
 
-
-_PUBLIC_EXPORTS: tuple[str, ...] = FLEXT_CLI_PUBLIC_EXPORTS
 
 __all__: tuple[str, ...] = (
     "FlextCli",
@@ -114,5 +171,5 @@ install_lazy_exports(
     __name__,
     globals(),
     _LAZY_IMPORTS,
-    public_exports=_PUBLIC_EXPORTS,
+    public_exports=__all__,
 )
