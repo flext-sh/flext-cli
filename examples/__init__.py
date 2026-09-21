b0b5fcb8abc3f018911ffa8707762557f14d9574
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-21 11:25:59 -0300

    chore(gen): converge projections with the published generator (d1c55b54f, one name per line)

--- examples/__init__.py
+++ examples/__init__.py
@@ -9,7 +9,8 @@ from typing import TYPE_CHECKING
 from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
-    from flext_cli import d, e, h, r, s, x
+    from flext_cli import s
+    from flext_core import d, e, h, r, x
 
     from . import _models_parts
     from .constants import ExamplesFlextCliConstants, ExamplesFlextCliConstants as c
@@ -22,11 +23,26 @@ if TYPE_CHECKING:
     from .typings import ExamplesFlextCliTypes, ExamplesFlextCliTypes as t
     from .utilities import ExamplesFlextCliUtilities, ExamplesFlextCliUtilities as u
 __all__: tuple[str, ...] = (
-    "DataManagerCLI", "Ex05Authentication", "Ex06Settings", "ExamplesFlextCliConstants",
-    "ExamplesFlextCliGettingStarted", "ExamplesFlextCliModels", "ExamplesFlextCliProtocols", "ExamplesFlextCliTypes",
-    "ExamplesFlextCliUtilities", "_models_parts", "c", "d",
-    "e", "h", "m", "p",
-    "r", "s", "t", "u",
+    "DataManagerCLI",
+    "Ex05Authentication",
+    "Ex06Settings",
+    "ExamplesFlextCliConstants",
+    "ExamplesFlextCliGettingStarted",
+    "ExamplesFlextCliModels",
+    "ExamplesFlextCliProtocols",
+    "ExamplesFlextCliTypes",
+    "ExamplesFlextCliUtilities",
+    "_models_parts",
+    "c",
+    "d",
+    "e",
+    "h",
+    "m",
+    "p",
+    "r",
+    "s",
+    "t",
+    "u",
     "x",
 )
 
@@ -43,7 +59,8 @@ _LAZY_IMPORTS = MappingProxyType(
             ".protocols": ("ExamplesFlextCliProtocols", "p"),
             ".typings": ("ExamplesFlextCliTypes", "t"),
             ".utilities": ("ExamplesFlextCliUtilities", "u"),
-            "flext_cli": ("d", "e", "h", "r", "s", "x"),
+            "flext_cli": ("s",),
+            "flext_core": ("d", "e", "h", "r", "x"),
         }),
         alias_groups=MappingProxyType({}),
         sort_keys=False,
