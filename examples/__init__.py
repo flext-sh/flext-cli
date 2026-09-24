d01a169cc91c69915cd4f4d2f6d4b62e78d794c4
Marlon Costa <marlon.costa@datacosmos.com.br>
2026-09-24 18:26:47 -0300

    chore(gen): absorb stale projections left by the pre-cure generator

--- examples/__init__.py
+++ examples/__init__.py
@@ -10,7 +10,17 @@ from flext_core.lazy import build_lazy_import_map, install_lazy_exports
 
 if TYPE_CHECKING:
     from flext_cli import cli, config, main, s, settings
-    from flext_core import core, d, e, h, lazy_attribute, r, services, x
+    from flext_core import (
+        core,
+        d,
+        e,
+        h,
+        lazy,
+        lazy_attribute,
+        normalize_lazy_imports,
+        r,
+        x,
+    )
 
     from . import _models_parts
     from .constants import ExamplesFlextCliConstants, c
@@ -42,13 +52,14 @@ __all__: tuple[str, ...] = (
     "d",
     "e",
     "h",
+    "lazy",
     "lazy_attribute",
     "m",
     "main",
+    "normalize_lazy_imports",
     "p",
     "r",
     "s",
-    "services",
     "settings",
     "t",
     "u",
@@ -74,9 +85,10 @@ _LAZY_IMPORTS = MappingProxyType(
                 "d",
                 "e",
                 "h",
+                "lazy",
                 "lazy_attribute",
+                "normalize_lazy_imports",
                 "r",
-                "services",
                 "x",
             ),
         }),
